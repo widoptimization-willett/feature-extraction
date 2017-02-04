3518e9088ecbbc273f922ba418d2962d6af2dda5
Liam Marshall <liam@cpan.org>
2017-02-04 16:47:18 -0600

    Add cell-boundary preprocessing to HaralickTexture measurement

--- feature_extraction/measurements/texture_haralick.py
+++ feature_extraction/measurements/texture_haralick.py
@@ -1,6 +1,29 @@
 from . import Measurement
 import feature_extraction.util.cleanup as cleanup
+from skimage.morphology import binary_erosion, disk
 
 class HaralickTexture(Measurement):
+	default_options = {
+		'clip_cell_borders': True,
+		'erode_cell': False,
+		'erode_cell_amount': False,
+	}
+	def __init__(self, options=None):
+		super(HaralickTexture, self).__init__(options)
+
 	def compute(self, image):
+		# -- preprocessing
+		if self.options.clip_cell_borders:
+			# get the cell boundary mask
+			mask = cleanup.cell_boundary_mask(image)
+
+			# if we're told to, erode the mask with a disk by some amount
+			if self.options.erode_cell:
+				mask = binary_erosion(cleanup.cell_boundary_mask(), disk(self.options.erode_cell_amount))
+
+			# mask the image
+			image = image[mask]
+
+		# -- haralick setup and run
+
 		return []
