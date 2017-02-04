a79a55502d4b4e4867d997cb80630181444a3274
Liam Marshall <liam@cpan.org>
2017-02-04 16:48:10 -0600

    Add PyYAML as a dep in preparation for pipeline manifests

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	license='Apache',
 
 	packages=['feature_extraction'],
-	install_requires=['numpy', 'Pillow', 'Click', 'scikit-image', 'centrosome'],
+	install_requires=['PyYAML', 'numpy', 'Pillow', 'Click', 'scikit-image', 'centrosome'],
 
 	entry_points='''
 	[console_scripts]
