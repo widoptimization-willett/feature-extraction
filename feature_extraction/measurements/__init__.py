02e76445b79dd49a900376619352ff8988b357d8
Liam Marshall <liam@cpan.org>
2017-02-03 19:31:35 -0600

    Measurements inherit from Measurment, which has an __init__(options=None)

---
+++ feature_extraction/measurements/__init__.py
@@ -0,0 +1,11 @@
+class Measurement(object):
+	"""
+	A generic feature measurement.
+	"""
+
+	def __init__(self, options=None):
+		"""
+		When initializing this measurement, options can be passed.
+		These are exposed to internal algorithms as `self.options`.
+		"""
+		self.options = options
--- feature_extraction/measurements/pixelaverage.py
+++ feature_extraction/measurements/pixelaverage.py
@@ -1,9 +1,11 @@
 import numpy as np
+from . import Measurement
 
-"""
-This is an incredibly basic example of a feature-extraction measurement
-(simply returning the average of all pixel values in the image).
-"""
-class PixelAverage(object):
+class PixelAverage(Measurement):
+	"""
+	This is an incredibly basic example of a feature-extraction measurement
+	(simply returning the average of all pixel values in the image).
+	"""
+
 	def compute(self, image):
 		return [np.average(image)]
