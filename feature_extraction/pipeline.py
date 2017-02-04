24615d560968465582127e33e52c3f038bb80bb4
Liam Marshall <liam@cpan.org>
2017-02-04 17:14:22 -0600

    Add pipeline manifest loading + a pipeline manifest

---
+++ feature_extraction/pipeline.py
@@ -0,0 +1,35 @@
+import yaml
+from . import measurements
+
+"""
+A 'pipeline' is just a python list containing a number of instantiated Measurement objects.
+
+This module can load them from a YAML manifest.
+"""
+
+def construct_from_manifest(file):
+	"""
+	Construct a pipeline from a YAML manifest
+
+	Parameters
+	----------
+	file : file
+		An open file handle to a YAML process manifest
+
+	Returns
+	-------
+	list
+		A list of instantiated Measurement objects.
+	"""
+
+	data = yaml.safe_load(file)
+
+	pipeline = []
+	for m in data['measurements']:
+		class_name = m.keys()[0]
+		options = m.values()[0]
+		_class = getattr(measurements, class_name)
+
+		pipeline.append(_class(options)) # instantiate w/ options, append to pipeline
+
+	return pipeline
