f8bbfa63708a9b9e489d1fd414d3da9511d7564f
Liam Marshall <liam@cpan.org>
2017-02-04 17:14:39 -0600

    Wire the CLI up to the new pipeline manifest stuff

--- feature_extraction/cli.py
+++ feature_extraction/cli.py
@@ -4,16 +4,15 @@ import click
 import numpy as np
 from PIL import Image
 
-from . import extraction
+from . import extraction, pipeline
 from .measurements import PixelAverage, HaralickTexture
 
-# TODO(liam): replace with CLI arguments or a manifest
-ENABLED_MEASURMENTS = [PixelAverage(), HaralickTexture()]
-
 @click.command()
+@click.argument('pipeline_manifest', required=True)
 @click.argument('files', nargs=-1, required=True) # unlimited number of args can be passed (eg. globbing)
 @click.option('-o', '--output', default='features.json')
-def extract_features(files, output):
+def extract_features(pipeline_manifest, files, output):
+	pipe = pipeline.construct_from_manifest(open(pipeline_manifest))
 	# TODO(liam): replace this with sending to celery and joining
 	processed_files = [] # array of maps containing metadata and feature vectors mapped to files
 	for filename in files:
@@ -22,7 +21,7 @@ def extract_features(files, output):
 		assert im.ndim == 2 # rank should be 2 if we're only considering grayscale images
 
 		# -- extract features
-		feature_vector = extraction.extract_features(im, ENABLED_MEASURMENTS)
+		feature_vector = extraction.extract_features(im, pipe)
 
 		# -- add the features + metadata to the output list
 		processed_files.append({'filename': filename,
