2c3501e1d240c40ea95fb80f8ecf28d89a4d1eec
Liam Marshall <liam@cpan.org>
2017-02-04 16:45:25 -0600

    Set up proper options passing and defaults for `Measurement`s

--- feature_extraction/measurements/__init__.py
+++ feature_extraction/measurements/__init__.py
@@ -1,14 +1,31 @@
+from collections import defaultdict
+from feature_extraction.util import DefaultAttributeDict
+
 class Measurement(object):
 	"""
 	A generic feature measurement.
+
+	Attributes
+	----------
+	default_options
+		Can be set by subclasses to set default option values
 	"""
+
+	default_options = {}
 
 	def __init__(self, options=None):
 		"""
 		When initializing this measurement, options can be passed.
 		These are exposed to internal algorithms as `self.options`.
+
+		Parameters
+		----------
+		options : dict
+			A dict of options for this measurement.
 		"""
-		self.options = options
+		self.options = DefaultAttributeDict()
+		self.options.update(self.default_options or {})
+		self.options.update(options or {})
 
 from .pixelaverage import PixelAverage
 from .texture_haralick import HaralickTexture
---
+++ feature_extraction/util/__init__.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+class AttributeDict(dict):
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+class DefaultAttributeDict(defaultdict):
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
