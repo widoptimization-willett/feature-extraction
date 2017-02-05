6788fca8d83b2e205eb97347bb03c3676811541f
Liam Marshall <liam@cpan.org>
2017-02-05 15:05:28 -0600

    Remove .utils.DefaultAttributeDict

--- feature_extraction/util/__init__.py
+++ feature_extraction/util/__init__.py
@@ -9,13 +9,3 @@ class AttributeDict(dict):
 
     def __setattr__(self, key, value):
         self[key] = value
-
-class DefaultAttributeDict(defaultdict):
-    def __getattr__(self, key):
-        try:
-            return self[key]
-        except KeyError:
-            raise AttributeError(key)
-
-    def __setattr__(self, key, value):
-        self[key] = value
