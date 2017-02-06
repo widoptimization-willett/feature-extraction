cf81e2db44345d1e4d54b511f37bc81857db64cd
Liam Marshall <liam@cpan.org>
2017-02-06 15:18:20 -0600

    Document HaralickTexture options

--- feature_extraction/measurements/texture_haralick.py
+++ feature_extraction/measurements/texture_haralick.py
@@ -6,6 +6,36 @@ from skimage.morphology import binary_erosion, disk
 from centrosome .haralick import Haralick
 
 class HaralickTexture(Measurement):
+	"""
+	This Measurement measures the texture of a image or cell by the Haralick method.
+	Code is derived from that in CellProfiler; CellProfiler's image library, Centrosome,
+	is used to perform the Haralick computations.
+
+	Options
+	---------------
+	scale : int
+		The scale at which to measure texture correlations.
+		This is the distance at which intensities are correlated.
+		
+		Default: 10 pixels.
+	angle : {'average', 'horizontal', 'vertical', 'diagonal', 'antidiagonal'}
+		The direction to measure intensity correlation in (that is, the direction
+		of the offset to chose pixels).
+		If set to `'average'`, the Haralick texture parameters are calculated for all directions
+		and then averaged, which produces a kind of rotational invariance.
+
+		Default: `'average'`.
+	clip_cell_borders : bool
+		When `True`, a RoI mask will be identified of the cell only.
+		The Haralick routines will then operate only on this region.
+
+		Defult: `True`
+	cell_border_erosion : int or None
+		If `clip_cell_borders` is `True`, the RoI mask will be eroded with a disk of size `cell_border_erosion.
+		May be useful if the cells have edge artifacting.
+
+		Default: `None`
+	"""
 	default_options = {
 		'scale': 10,
 		'angle': 'average',
