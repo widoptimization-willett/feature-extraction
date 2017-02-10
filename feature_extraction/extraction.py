832256925a08c0bbfe3f507ef613d51e11ac8103
Liam Marshall <liam@cpan.org>
2017-02-10 17:15:09 -0600

    Add a function to normalize the feature vectors

--- feature_extraction/extraction.py
+++ feature_extraction/extraction.py
@@ -10,3 +10,13 @@ def extract_features(image, measurements):
 	# TODO(liam): parallelize multiple measurements on an image by using Celery
 	
 	return np.hstack([m.compute(image) for m in measurements])
+
+def normalize_features(X):
+	# recenter features and normalize over the dataset
+	X -= np.mean(X, axis=0)
+	X /= np.linalg.norm(X, axis=0)
+
+	# normalize for each record
+	X /= np.vstack(np.linalg.norm(X, axis=1))
+
+	return X
