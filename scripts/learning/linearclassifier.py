f5139a937eb6e0327e12ff63238c87b7a4f98a88
Liam Marshall <liam@cpan.org>
2017-02-28 11:52:44 -0600

    Check in linear classifier and feature-vector testing code

---
+++ scripts/learning/linearclassifier.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+"""
+linear classification and verification
+"""
+
+def train_weights(l, X, Y):
+	"""
+	Parameters
+	----------
+	l : float
+		label error coefficient
+	X : array_like [[float, ...], ...]
+		vector of feature vectors in training set
+	Y : array_like [int, int, ...]
+		vector of integer labels
+	
+	Returns
+	-------
+	theta_hat : array_like [float, float, ...]
+		the model weights
+	"""
+	
+	return np.dot(np.dot(np.linalg.inv(np.dot(X.T, X) + l*np.identity(X.shape[1])), X.T), Y)
+
+def predict(X, weights):
+	return np.dot(X, weights)
+
+def prediction_errors(Y, _Y):
+	_Y_clipped = (_Y>0)*2-1
+	return float(np.count_nonzero(Y-_Y_clipped))
+
+def find_optimal_weights(L, X_train, Y_train, X_verification, Y_verification):
+	errors = [prediction_errors(Y_verification,
+								predict(X_verification, train_weights(l, X_train, Y_train))) for l in L]
+
+	best_l = L[np.argmin(errors)]
+	best_weights = train_weights(best_l, X_train, Y_train)
+	
+	return (best_weights, best_l)
---
+++ scripts/verify_features.py
@@ -0,0 +1,45 @@
+import json
+import sys
+import random
+import numpy as np
+from itertools import chain, izip_longest
+from tqdm import tqdm
+import learning.linearclassifier as linearclassifier
+
+
+def class_from_filename(n):
+	if 'vlp' in n:
+		return 'vlp'
+	elif 'diffuse' in n:
+		return 'diffuse'
+
+def encode_class(c):
+	return {'vlp': 1, 'diffuse': -1}[c]
+
+def extract_xy(db):
+	return np.array(zip(*db)[1]), np.array(map(encode_class, zip(*db)[0]))
+
+def shuffle(a, b):
+	return filter(None, chain(*izip_longest(a, b)))
+
+with open(sys.argv[1]) as f:
+	featurefile = json.load(f)
+
+featuredb = [(class_from_filename(filen), x) for (filen, x) in featurefile.items()]
+random.shuffle(featuredb)
+
+splitpoint = len(featuredb)/2
+overlap = 10
+trainingdb = featuredb[0:splitpoint+overlap]
+verificationdb = featuredb[splitpoint-overlap:]
+
+X, Y = extract_xy(trainingdb)
+# print X.shape, Y.shape, len(trainingdb)
+X_v, Y_v = extract_xy(verificationdb)
+
+th, l = linearclassifier.find_optimal_weights(np.linspace(0, 10, 200), X, Y, X_v, Y_v)
+print "optimum lambda={}; found theta_hat={}".format(l, th)
+
+errno = linearclassifier.prediction_errors(Y_v, linearclassifier.predict(X_v, th))
+
+print "classification error: #={}, %={}".format(errno, 100.0*errno/len(X_v))
